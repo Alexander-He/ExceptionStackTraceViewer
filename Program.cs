7a831ed41f87696ba05cb3649bfaa8f87ebaf7eb
Alexander He <AlexanderHe@Seismic.com>
2019-03-22 12:57:39 +0800

    set icon

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ExceptionStackTraceViewer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ExceptionStackTraceViewer());
         }
     }
 }
