1e93440800540086bcea02e2387929520fe4e2c0
Alexander He <AlexanderHe@Seismic.com>
2019-03-22 14:39:49 +0800

    rename

--- ExceptionStackTraceViewer.Designer.cs
+++ ExceptionStackTraceViewer.Designer.cs
@@ -41,7 +41,7 @@
             this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1695, 79);
+            this.textBox1.Size = new System.Drawing.Size(1695, 46);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Paste stack trace  here ......";
             this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
@@ -50,18 +50,18 @@
             // 
             // textBox2
             // 
-            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.Color.Blue;
-            this.textBox2.Location = new System.Drawing.Point(0, 85);
+            this.textBox2.Location = new System.Drawing.Point(0, 46);
             this.textBox2.MaxLength = 1999999999;
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox2.Size = new System.Drawing.Size(1695, 703);
+            this.textBox2.Size = new System.Drawing.Size(1695, 742);
             this.textBox2.TabIndex = 1;
             // 
-            // Form1
+            // ExceptionStackTraceViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -69,7 +69,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.Name = "ExceptionStackTraceViewer";
             this.Text = "ExceptionStackTraceViewer";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
